fb45b1d6b932d57a9c2637c62219d064f26c3ee1
victorfdez <vicfernandezespana@gmail.com>
2024-10-03 19:19:26 +0200

    ejercicios y apuntes

---
+++ Python/02-Variables/Ejercicio2.py
@@ -0,0 +1,8 @@
+""" 
+    2. Inicializa una variable (es decir, crea una variable con un identificador determinado y dale un valor) de tipo número real e imprime dicha variable en pantalla (se tendrán que ver el valor de esa variable).
+    @author Victor Fernandez España
+
+"""
+variable = 33
+
+print(variable)
